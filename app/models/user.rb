e18106f0ebf265ec9b590f866d2dce6e339493f6
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-12-31 00:19:46 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ActiveRecord::Base
   has_secure_password
   
+  def display_balance
+      @balance
+  end
   
+  def deposit(amount)
+      @balance += amount
+  end
+  
+  def withdraw(amount)
+    if !amount > @balance
+      @balance -= amount
+    else
+      raise.error 
+    end
+  end
 end
